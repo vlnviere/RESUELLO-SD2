3d7fccf3ecf0ddb42c3e5dc8f328486ee05126d1
vlnviere <jsresuello@gmail.com>
2026-03-12 11:53:45 +0800

    JS Update

---
+++ session14/index.js
@@ -0,0 +1,59 @@
+// Single line comment using '//'
+// console.log is a function that prints a message to the console.
+console.log("Hello, World!");
+
+/*
+This
+Is
+A
+Multi-line comment
+*/
+
+// [Section] Variables
+// Variables are used to store data. In JavaScript, we can declare variables using 'var', 'let', or 'const'.
+
+//[Section] Data Types
+// Numberic data types 
+/*
+Integer: Whole numbers without a decimal point
+
+Floating-point: Numbers with a decimal point
+
+Double: A double-precision floating-point number, which can represent a wider range of values than a regular floating-point number
+*/
+
+let age = 25; // This is a number
+let height = 5.9; // This is a floating-point number
+
+console.log(height)
+//JS is a loose typed language, meaning you can change the type of a variable after it has been declared.
+
+// Syncronous vs Asynchronous
+// Syncronous code is executed in order, one line at a time. Asynchronous code allows for non-blocking operations, meaning that the code can continue to execute while waiting for an operation to complete.
+
+let gwa = 99.75;
+console.log(gwa);
+
+//Character data types
+/*
+String: A sequence of characters enclosed in single or double quotes
+Char: A single character enclosed in single or double quotes
+*/
+
+let firstName = "John"; // This is a string
+console.log(firstName);
+
+//Boolean data types
+/*
+Boolean: A data type that can only have two values: true or false
+*/
+
+let isStudent = true; // This is a boolean
+console.log(isStudent);
+
+//Concatenation
+// Concatenation is the process of combining two or more strings together. In JavaScript, we can use the '+' operator to concatenate strings.
+
+let lastName = "Doe";
+let fullName = firstName + " " + lastName; // This is string concatenation
+console.log(fullName);
