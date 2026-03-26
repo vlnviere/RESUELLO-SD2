96457867f7407a2a0a104982b52b92a285b4a217
vlnviere <jsresuello@gmail.com>
2026-03-26 11:16:27 +0800

    Updated

---
+++ session18/index.js
@@ -0,0 +1,17 @@
+console.log("Hello World!");
+//JS Functions - performs specific task and eliminates repetitive code
+//function functionName(parameters){code block}
+
+console.log("Hello World!");
+
+function sayHello(){
+    console.log("Hello, San Fernando City, Pampanga!");
+}
+
+//Invocation/Invoke - calling a function
+sayHello(); //reusable code, can be called multiple times
+sayHello();
+sayHello();
+
+
+
