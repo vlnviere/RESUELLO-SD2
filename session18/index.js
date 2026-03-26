f57c5cd0e9633221d7d140295fb634b8930335c8
vlnviere <jsresuello@gmail.com>
2026-03-26 11:37:40 +0800

    Session18 Added

--- session18/index.js
+++ session18/index.js
@@ -13,5 +13,25 @@ sayHello(); //reusable code, can be called multiple times
 sayHello();
 sayHello();
 
+//showAlert(); //error, function not yet declared
+function showAlert(){
+    alert("This is an alert box!");
+}
 
+//startCalc(); //error, function not yet declared
+
+let num1 = 0 , num2 = 0;
+function startCalc(){
+    let userInput = parseInt(prompt("Please select an operation: \n[1]Input Numbers \n[2]Sum \n[3]Difference \n[4]Product \n[5]Quotient [6]Reset"));
+
+    switch(userInput){
+        case 1:
+            sum();
+            break;
+    }
+}   
+
+function sum(num1, num2){
+    console.log(`The sum of ${num1} and ${num2} is:`, num1 + num2);
+}
 
